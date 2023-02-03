c1ec32813d130a5a59e312e153dc52ae5007febe
mustafahincal <mustafahncal@gmail.com>
2023-02-03 22:33:41 +0300

    update Product page, add careousel

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,6 +1,25 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const Product = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
   return (
     <>
       <div className="w-4/5 m-auto mt-10">
@@ -158,16 +177,137 @@ const Product = () => {
       </div>
 
       <div className="w-4/5 m-auto py-16 ">
-        <div className="text-product-page-sponsored font-semibold text-[30px]">
+        <div className="text-gray-500 font-semibold text-[25px]">
           Smiliar sponsored items
         </div>
         <div className="mt-10">
-          <div className="w-[300px] h-[370px] flex flex-col rounded-3xl p-7 bg-white shadow-sponsored-item">
-            <div className="w[250px] h-[220px] flex justify-center items-center rounded-3xl bg-product-image-bg">
-              <img src="./assets/sponsored-item1.svg" alt="" />
+          <Carousel
+            swipeable={true}
+            draggable={true}
+            infinite={true}
+            autoPlay={true}
+            responsive={responsive}
+            keyBoardControl={true}
+            customTransition="all .5"
+            transitionDuration={500}
+            containerClass="carousel-container"
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px"
+          >
+            <div className="w-[250px] h-[340px] flex flex-col rounded-3xl p-5 bg-white shadow-sponsored-item gap-0.5">
+              <div className="w[200px] h-[200px] flex justify-center items-center rounded-3xl bg-product-image-bg">
+                <img src="./assets/sponsored-item1.svg" alt="" />
+              </div>
+              <span className="text-gray-600 mt-3">iPhone 12 Charger...</span>
+              <span className="text-gray-500">Apple</span>
+              <div className="flex justify-between items-center">
+                <span className="font-medium text-[24px] ">13.90$</span>
+                <div className="flex gap-2 items-center">
+                  <img src="./assets/product-star.svg" alt="" />
+                  <span>4.5</span>
+                </div>
+              </div>
             </div>
-            <span>iPhone 12 Charger...</span>
-          </div>
+            <div className="w-[250px] h-[340px] flex flex-col rounded-3xl p-5 bg-white shadow-sponsored-item gap-0.5">
+              <div className="w[200px] h-[200px] flex justify-center items-center rounded-3xl bg-product-image-bg">
+                <img src="./assets/sponsored-item2.svg" alt="" />
+              </div>
+              <span className="text-gray-600 mt-3">iPhone 12 Charger...</span>
+              <span className="text-gray-500">Apple</span>
+              <div className="flex justify-between items-center">
+                <span className="font-medium text-[24px] ">13.90$</span>
+                <div className="flex gap-2 items-center">
+                  <img src="./assets/product-star.svg" alt="" />
+                  <span>4.5</span>
+                </div>
+              </div>
+            </div>
+            <div className="w-[250px] h-[340px] flex flex-col rounded-3xl p-5 bg-white shadow-sponsored-item gap-0.5">
+              <div className="w[200px] h-[200px] flex justify-center items-center rounded-3xl bg-product-image-bg">
+                <img src="./assets/sponsored-item3.svg" alt="" />
+              </div>
+              <span className="text-gray-600 mt-3">iPhone 12 Charger...</span>
+              <span className="text-gray-500">Apple</span>
+              <div className="flex justify-between items-center">
+                <span className="font-medium text-[24px] ">13.90$</span>
+                <div className="flex gap-2 items-center">
+                  <img src="./assets/product-star.svg" alt="" />
+                  <span>4.5</span>
+                </div>
+              </div>
+            </div>
+            <div className="w-[250px] h-[340px] flex flex-col rounded-3xl p-5 bg-white shadow-sponsored-item gap-0.5">
+              <div className="w[200px] h-[200px] flex justify-center items-center rounded-3xl bg-product-image-bg">
+                <img src="./assets/sponsored-item4.svg" alt="" />
+              </div>
+              <span className="text-gray-600 mt-3">iPhone 12 Charger...</span>
+              <span className="text-gray-500">Apple</span>
+              <div className="flex justify-between items-center">
+                <span className="font-medium text-[24px] ">13.90$</span>
+                <div className="flex gap-2 items-center">
+                  <img src="./assets/product-star.svg" alt="" />
+                  <span>4.5</span>
+                </div>
+              </div>
+            </div>
+            <div className="w-[250px] h-[340px] flex flex-col rounded-3xl p-5 bg-white shadow-sponsored-item gap-0.5">
+              <div className="w[200px] h-[200px] flex justify-center items-center rounded-3xl bg-product-image-bg">
+                <img src="./assets/sponsored-item1.svg" alt="" />
+              </div>
+              <span className="text-gray-600 mt-3">iPhone 12 Charger...</span>
+              <span className="text-gray-500">test1</span>
+              <div className="flex justify-between items-center">
+                <span className="font-medium text-[24px] ">13.90$</span>
+                <div className="flex gap-2 items-center">
+                  <img src="./assets/product-star.svg" alt="" />
+                  <span>4.5</span>
+                </div>
+              </div>
+            </div>
+            <div className="w-[250px] h-[340px] flex flex-col rounded-3xl p-5 bg-white shadow-sponsored-item gap-0.5">
+              <div className="w[200px] h-[200px] flex justify-center items-center rounded-3xl bg-product-image-bg">
+                <img src="./assets/sponsored-item1.svg" alt="" />
+              </div>
+              <span className="text-gray-600 mt-3">iPhone 12 Charger...</span>
+              <span className="text-gray-500">test2</span>
+              <div className="flex justify-between items-center">
+                <span className="font-medium text-[24px] ">13.90$</span>
+                <div className="flex gap-2 items-center">
+                  <img src="./assets/product-star.svg" alt="" />
+                  <span>4.5</span>
+                </div>
+              </div>
+            </div>
+            <div className="w-[250px] h-[340px] flex flex-col rounded-3xl p-5 bg-white shadow-sponsored-item gap-0.5">
+              <div className="w[200px] h-[200px] flex justify-center items-center rounded-3xl bg-product-image-bg">
+                <img src="./assets/sponsored-item1.svg" alt="" />
+              </div>
+              <span className="text-gray-600 mt-3">iPhone 12 Charger...</span>
+              <span className="text-gray-500">Apple</span>
+              <div className="flex justify-between items-center">
+                <span className="font-medium text-[24px] ">13.90$</span>
+                <div className="flex gap-2 items-center">
+                  <img src="./assets/product-star.svg" alt="" />
+                  <span>4.5</span>
+                </div>
+              </div>
+            </div>
+            <div className="w-[250px] h-[340px] flex flex-col rounded-3xl p-5 bg-white shadow-sponsored-item gap-0.5">
+              <div className="w[200px] h-[200px] flex justify-center items-center rounded-3xl bg-product-image-bg">
+                <img src="./assets/sponsored-item1.svg" alt="" />
+              </div>
+              <span className="text-gray-600 mt-3">iPhone 12 Charger...</span>
+              <span className="text-gray-500">Apple</span>
+              <div className="flex justify-between items-center">
+                <span className="font-medium text-[24px] ">13.90$</span>
+                <div className="flex gap-2 items-center">
+                  <img src="./assets/product-star.svg" alt="" />
+                  <span>4.5</span>
+                </div>
+              </div>
+            </div>
+          </Carousel>
         </div>
       </div>
     </>
